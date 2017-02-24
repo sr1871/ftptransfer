bf0b3efdfb6e623291ed17d9f86e90d6f5a6348b
Sergio Alvarado Villareal (Becario) <sergioal18v@gmail.com>
2017-02-24 12:35:54 -0600

    se creo el funcionamiento de ftp-transfer

--- FtpTransfer.php
+++ FtpTransfer.php
@@ -1,4 +1,3 @@
-
 <?php
 
 namespace sr1871\ftptransfer; 
@@ -6,8 +5,15 @@ namespace sr1871\ftptransfer;
 /** 
  * ftptransfer module definition class 
  */ 
-class FtpTransfer extends \yii\base\Module 
-{ 
+class FtpTransfer extends \yii\base\Module { 
+    public $ip;
+    public $user;
+    public $password;
+    public $port;
+    public $server;
+    public $_conn;
+    public $pasive = false;
+    
     /** 
      * @inheritdoc 
      */ 
@@ -15,10 +21,62 @@ class FtpTransfer extends \yii\base\Module
     /** 
      * @inheritdoc 
      */ 
-    public function init() 
-    { 
+    public function init() { 
         parent::init(); 
 
-        // custom initialization code goes here 
+        $this->_conn = ftp_connect($this->server) or die("Couldn't connect to $ftp_server");         // custom initialization code goes here 
+        if (@ftp_login($this->_conn, $this->user, $this->password)) {
+            @ftp_pasv($this->_conn, $this->pasive);
+        } 
+
+       
     } 
+        
+    public function _close() {
+        @ftp_close($this->_conn);
+    }
+
+    public function getFile($remotePath, $localPath) {
+        $result = @ftp_nb_get($this->_conn, $localPath, $remotePath , FTP_BINARY);
+        ftp_close($this->_conn);
+        return $result;
+
+    }
+
+    public function put($remotePath, $localPath) {
+        $result = @ftp_nb_put($this->_conn, $remotePath, $localPath, FTP_BINARY);
+        ftp_close($this->_conn);
+        return $result;
+    }
+
+    public function mkdir($directory, $permissions=777) {
+        $result = @ftp_mkdir($this->_conn, $directory);
+        if ($result != 0) {
+            $this->changePermissions($permissions, $directory);
+        }
+        ftp_close($this->_conn);
+        return $result;
+    }
+
+    public function delete($remotePath) {
+        $result = @ftp_delete($this->_conn, $remotePath);
+        ftp_close($this->_conn);
+        return $result;
+    }
+
+    private function chmod($permissions, $file) {
+        $result = @ftp_chmod($this->_conn, $permissions, $file);
+        ftp_close($this->_conn);
+        return $result;
+    }
+
+    public function rmdir($directory) {
+        $result = @ftp_rmdir($this->_conn, $directory);
+        ftp_close($this->_conn);
+        return $result;
+    }
+
+    public function changePermissions($permissions, $file) {
+        $this->chmod($permissions,$file);
+    }
 } 
